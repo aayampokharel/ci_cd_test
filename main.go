41c03aa25df7d406d9c57ae2d502b3719ce6247f
aayam pokharel <aayam.pokharel@gmail.com>
2025-12-21 00:19:49 +0545

    fix(main.go): deliberate error code to test error in workflow

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import "fmt"
 
-func addTwoNumbers(a, b int) int {
-	return a + b
-}
+
 func main() {
 	fmt.Print("====main() started ====")
-	addTwoNumbers(1, 2)
-	fmt.Print("====main() end ====")
+	
+	fmt.Print("====main() end 
 }
