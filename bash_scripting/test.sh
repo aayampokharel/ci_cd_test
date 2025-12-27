0398bd90537aa8b2d2641977083d471c7078ae7a
aayam pokharel <aayam.pokharel@gmail.com>
2025-12-27 23:43:17 +0545

    fix(sh): argument passing

--- bash_scripting/test.sh
+++ bash_scripting/test.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
-echo "Who are you,stranger?"
-read name
-echo "you are $name"
-readonly PI=3.145
-today=$(whoami)
-echo "today:$today"
+workout=$(wc -l < $1)
+echo "result of line count from $1 is $workout"
