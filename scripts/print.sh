636f2dc936d4003e5a5da2af208f34a4db6dbf71
aayam pokharel <aayam.pokharel@gmail.com>
2026-01-08 23:37:20 +0545

    fix(sh): if else testing

--- scripts/print.sh
+++ scripts/print.sh
@@ -1,5 +1,7 @@
-#! /bin/bash
 
-path_env="hello from another world"
-export path_env
-echo $path_env
+if [[ $(ls -d $1 2> /dev/null) = $1 ]]; then
+    echo "directory exists"
+else 
+echo "directory doesnot exist"
+
+fi
