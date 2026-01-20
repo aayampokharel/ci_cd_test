08d71f1c71c53be6a080cd94a8ab0e7d89e9e815
aayam pokharel <aayam.pokharel@gmail.com>
2026-01-20 18:55:47 +0545

    fix(yml): testing with env passage and using in GO code

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Println("=======start=========")
 	fmt.Println(os.Getenv("DB_PASS"))
 	fmt.Println(os.Getenv("PASS_FROM_SECRETS"))
 	fmt.Println("=======end=========")
